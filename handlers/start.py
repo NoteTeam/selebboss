2773f396e52244d70f1255d014713c1e79552668
NoteTeam <90462419+NoteTeam@users.noreply.github.com>
2021-09-25 23:01:00 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -61,10 +61,10 @@ async def start_(client: Client, message: Message):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🌐 Wiki's Page", url="https://github.com/NoteTeam/Clover/wiki/Clover-Music-Wiki's")
+                        "🌐 Wiki's Page", url="https://github.com/NoteTeam/selebboss/wiki/selebboss-Music-Wiki's")
                 ],[
                     InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/NoteTeam/Clover"
+                        "🧪 Source Code 🧪", url="https://github.com/NoteTeam/selebboss"
                     )
                 ]
             ]
